2cfa840f5cfb4dee92975f9dce020ee7e972918c
whoslcy <whoslcy@foxmail.com>
2023-06-25 17:55:00 +0800

    Complete assignment1

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,8 +1,9 @@
 #include "Triangle.hpp"
 #include "rasterizer.hpp"
-#include <eigen3/Eigen/Eigen>
+#include <Eigen/Eigen>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <cmath>
 
 constexpr double MY_PI = 3.1415926;
 
@@ -26,7 +27,14 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
-
+    float rotation_radian = rotation_angle / 180 * MY_PI;
+    float cosine_value = std::cos(rotation_radian);
+    float sine_value = std::sin(rotation_radian);
+    Eigen::Matrix2f rotation_matrix {
+        {cosine_value, -sine_value},
+        {sine_value, cosine_value},
+    };
+    model.topLeftCorner<2, 2>() = rotation_matrix;
     return model;
 }
 
@@ -40,8 +48,46 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    Eigen::Matrix4f perspective_to_orthographic {
+        {zNear, 0, 0, 0},
+        {0, zNear, 0, 0},
+        {0, 0, zNear + zFar, -zNear * zFar},
+        {0, 0, 1, 0},
+    };
+    float yTop = 2 * std::abs(zNear) * std::tan(eye_fov / 2);
+    float yBottom = -yTop;
+    float xRight = aspect_ratio * yTop;
+    float xLeft = -xRight;
+    Eigen::Matrix4f orthographic_scale {
+        {2 / (xRight - xLeft), 0, 0, 0},
+        {0, 2 / (yTop - yBottom), 0, 0},
+        {0, 0, 2 / (zNear - zFar), 0},
+        {0, 0, 0, 1},
+    };
+    Eigen::Matrix4f orthographic_translate {
+        {1, 0, 0, -(xRight + xLeft) / 2},
+        {0, 1, 0, -(yTop + yBottom) / 2},
+        {0, 0, 1, -(zNear + zFar) / 2},
+        {0, 0, 0, 1},
+    };
+    return orthographic_scale * orthographic_translate * perspective_to_orthographic;
+}
 
-    return projection;
+Eigen::Matrix4f get_rotation(Eigen::Vector3f axis, float rotation_angle){
+    Eigen::Matrix4f rodrigues_rotation = Eigen::Matrix4f::Identity();
+    float rotation_radian = rotation_angle / 180 * MY_PI;
+    float cosine_value = std::cos(rotation_radian);
+    float sine_value = std::sin(rotation_radian);
+    float n_x = axis(0);
+    float n_y = axis(1);
+    float n_z = axis(2);
+    Eigen::Matrix3f N {
+        {0, -n_z, n_y},
+        {n_z, 0, -n_x},
+        {-n_y, n_x, 0},
+    };
+    rodrigues_rotation.topLeftCorner<3, 3>() = cosine_value * Eigen::Matrix3f::Identity() + (1 - cosine_value) * axis * axis.transpose() + sine_value * N;
+    return rodrigues_rotation;
 }
 
 int main(int argc, const char** argv)
@@ -93,7 +139,8 @@ int main(int argc, const char** argv)
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        // zNear and zFar should be negative.
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
